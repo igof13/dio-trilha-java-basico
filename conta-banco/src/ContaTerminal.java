3296368f0ae645633543880d062236c35bf4bf0d
Igor Fugagnoli <igor@milviz.com>
2025-02-19 11:31:26 -0300

    Desafio completo.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Testando");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entre seu nome.");
+        String nome = scanner.nextLine();
+
+        System.out.println("Entre sua Agência.");
+        int numAgencia = scanner.nextInt();
+
+        System.out.println("Entre seu número de conta bancária.");
+        int numContaBancaria = scanner.nextInt();
+
+        System.out.println("Entre seu saldo.");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + numAgencia + ", conta " + numContaBancaria + " e seu saldo " + saldo + " já está disponível para saque");
+        scanner.close();
     }
 }
